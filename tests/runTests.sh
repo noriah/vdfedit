a723d13e1946869db487c244b4267caa9de0b64b
noriah <vix@noriah.dev>
2013-12-04 18:40:28 -0600

    Updated Tests

--- tests/runTests.sh
+++ tests/runTests.sh
@@ -24,10 +24,9 @@ So all these results are going to fail.
 Just compare the ouputs to the files in the test/files folder."; }
 
 vdfedit="../vdfedit"
-out="./out/"
 
-rm -rf $out>/dev/null
-mkdir $out
+rm -rf out/>/dev/null
+mkdir out
 
 chmod +x $vdfedit
 
@@ -41,8 +40,8 @@ $waitcmd
 echo "This test will check that we are able to parse vdf files correctly."
 $waitcmd
 #Will Output All Values in files/localconfig.vdf
-$vdfedit files/localconfig.vdf >> out/localconfig.vdf.log
-output=`diff files/localconfig.vdf.log.check out/localconfig.vdf.log`
+$vdfedit in/localconfig.vdf >> out/localconfig.vdf.log
+output=`diff in/localconfig.vdf.log.check out/localconfig.vdf.log`
 test_results $output
 
 echo
@@ -52,7 +51,7 @@ echo "This test will check that minimal error checking works."
 echo "Should return:"
 echo "Something went wrong near here: CauseError.NoErrorYet.NoErrorYet.OhhhWeHitAnError"
 $waitcmd
-$vdfedit files/causeerror.vdf
+$vdfedit in/causeerror.vdf
 echo "Did It?"
 $waitcmd
 
@@ -61,46 +60,18 @@ echo
 echo "This test will check that we can edit a vdf"
 $waitcmd
 #Will Create file out/localconfig1.vdf which will have 2 new Values, near the bottom
-$vdfedit files/localconfig.vdf,out/localconfig1.vdf \
+$vdfedit in/localconfig.vdf,out/localconfig1.vdf \
 UserLocalConfigStore.shortcuts.TestEntry.Var=Test1,\
 UserLocalConfigStore.shortcuts.TestEntry.Var2=Test2
-output=`diff files/localconfig1.vdf.check out/localconfig1.vdf`
+output=`diff in/localconfig1.vdf.check out/localconfig1.vdf`
 test_results $output
 
 echo
 echo "This test will check that we can crate a new vdf"
 $waitcmd
-$vdfedit out/created.vdf \
-TestSet.Test5.Test4.Test3.Test2.Test1.Test=Test,\
-TestSet.Test4.Test3.Test2.Test1.Test=Test,\
-TestSet.Test3.Test2.Test1.Test=Test,\
-TestSet.Test2.Test1.Test=Test,\
-TestSet.Test1.Test=Test,\
-TestSet.Test5.Test4.Test3.Test2.Test=Test,\
-TestSet.Test4.Test3.Test2.Test=Test,\
-TestSet.Test3.Test2.Test=Test,\
-TestSet.Test2.Test=Test,\
-TestSet.Test5.Test4.Test3.Test=Test,\
-TestSet.Test4.Test3.Test=Test,\
-TestSet.Test3.Test=Test,\
-TestSet.Test5.Test4.Test2.Test1.Test=Test,\
-TestSet.Test4.Test2.Test1.Test=Test,\
-TestSet.Test5.Test4.Test2.Test=Test,\
-TestSet.Test4.Test2.Test=Test,\
-TestSet.Test5.Test4.Test1.Test=Test,\
-TestSet.Test4.Test1.Test=Test,\
-TestSet.Test5.Test4.Test=Test,\
-TestSet.Test4.Test=Test,\
-TestSet.Test5.Test3.Test2.Test1.Test=Test,\
-TestSet.Test5.Test3.Test2.Test=Test,\
-TestSet.Test5.Test3.Test1.Test=Test,\
-TestSet.Test5.Test3.Test=Test,\
-TestSet.Test5.Test2.Test1.Test=Test,\
-TestSet.Test5.Test2.Test=Test,\
-TestSet.Test5.Test1.Test=Test,\
-TestSet.Test5.Test=Test
+$vdfedit out/created.vdf < in/create.vdfe
 echo
-output=`diff files/created.vdf.check out/created.vdf`
+output=`diff in/created.vdf.check out/created.vdf`
 test_results $output
 
 echo "Tests Complete"
