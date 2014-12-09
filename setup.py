4f6cade7c351e59236ac934dc0ce8b9172cdcba8
noriah <vix@noriah.dev>
2014-12-09 14:51:32 -0600

    Changes. Hope this works

--- setup.py
+++ setup.py
@@ -1,29 +1,26 @@
 from setuptools import setup
-# from setuptools.command.test import test as TestCommand
-# import sys
-# import codecs
-# import os
+from setuptools.command.test import test as TestCommand
+import sys
+import codecs
+import os
 
-# class PyTest(TestCommand):
-#     def finalize_options(self):
-#         TestCommand.finalize_options(self)
-#         self.test_args = []
-#         self.test_suite = True
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
 
-#     def run_tests(self):
-#         import subprocess
-#         p = subprocess.Popen(['./run_tests.sh'],
-#                              stdout=sys.stdout,
-#                              stderr=sys.stderr)
-#         p.communicate()
-#         sys.stdout.flush()
-#         sys.exit(p.returncode)
+    def run_tests(self):
+        from subprocess import call
+        p = call('./run_tests.sh', shell=True)
+        sys.exit(p)
 
 setup(
     name='vdfedit',
     version='1.0.0',
     scripts=['vdfedit'],
     install_requires=['PyVDF>=1.0.4'],
+    tests_require=['pytest'],
     license='MIT',
     url='https://github.com/noriah/vdfedit',
     author='noriah',
@@ -31,7 +28,7 @@ setup(
     keywords = "VDF KeyValues Valve PyVDF",
     description='Fast and Easy Python Valve Data File (VDF) Reader and Writer',
     platforms='any',
-    # cmdclass={'test': PyTest},
+    cmdclass={'test': PyTest},
     classifiers = [
         'Development Status :: 4 - Beta',
         'Environment :: Console',
