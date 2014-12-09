3a9c37e8213d9d6b8d3bf1166b1fc955afebcc54
noriah <vix@noriah.dev>
2014-12-09 02:03:43 -0600

    Version 1.0.0

--- setup.py
+++ setup.py
@@ -1,25 +1,23 @@
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
-import sys
-import codecs
-import os
+# from setuptools.command.test import test as TestCommand
+# import sys
+# import codecs
+# import os
 
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
+# class PyTest(TestCommand):
+#     def finalize_options(self):
+#         TestCommand.finalize_options(self)
+#         self.test_args = []
+#         self.test_suite = True
 
-    def run_tests(self):
-        import subprocess
-        # import pandoc
-        import PyVDF
-        p = subprocess.Popen(['./run_tests.sh'],
-                             stdout=sys.stdout,
-                             stderr=sys.stderr)
-        p.communicate()
-        sys.stdout.flush()
-        sys.exit(p.returncode)
+#     def run_tests(self):
+#         import subprocess
+#         p = subprocess.Popen(['./run_tests.sh'],
+#                              stdout=sys.stdout,
+#                              stderr=sys.stderr)
+#         p.communicate()
+#         sys.stdout.flush()
+#         sys.exit(p.returncode)
 
 setup(
     name='vdfedit',
@@ -33,7 +31,7 @@ setup(
     keywords = "VDF KeyValues Valve PyVDF",
     description='Fast and Easy Python Valve Data File (VDF) Reader and Writer',
     platforms='any',
-    cmdclass={'test': PyTest},
+    # cmdclass={'test': PyTest},
     classifiers = [
         'Development Status :: 4 - Beta',
         'Environment :: Console',
