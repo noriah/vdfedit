3f791eae115c38e6edfc6259222996ce70c2678b
noriah <vix@noriah.dev>
2014-12-09 16:33:55 -0600

    Update Travis. no more coveralls.

--- setup.py
+++ setup.py
@@ -1,26 +1,10 @@
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
-import sys
-import codecs
-import os
-
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        from subprocess import call
-        p = call('./run_tests.sh', shell=True)
-        sys.exit(p)
 
 setup(
     name='vdfedit',
     version='1.0.0',
     scripts=['vdfedit'],
     install_requires=['PyVDF>=1.0.4'],
-    tests_require=['pytest'],
     license='MIT',
     url='https://github.com/noriah/vdfedit',
     author='noriah',
@@ -28,7 +12,6 @@ setup(
     keywords = "VDF KeyValues Valve PyVDF",
     description='Fast and Easy Python Valve Data File (VDF) Reader and Writer',
     platforms='any',
-    cmdclass={'test': PyTest},
     classifiers = [
         'Development Status :: 4 - Beta',
         'Environment :: Console',
